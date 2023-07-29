cb643bc75f7cb637902f3e03f7e35e5ecc9f91af
José Esteves <joseesteves199930@gmail.com>
2023-07-29 13:21:05 -0400

    feat: :sparkles: logo y comodo de tamaño de rem
    se creo el logo del sistema y se acomodo el tamaño echo cn rem

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react';
 import BtnTheme from './BtnTheme';
 import FormLogin from './FormLogin';
 import './login.css';
-import { IconUser } from './Icons';
+import { IconUser, Logo } from './Icons';
 
 const Login = () => {
 	const [loading, setLoading] = useState(false);
 	return (
 		<section className='login'>
 			<div className='login__iconUser'>
+				<h1
+					className='login__logo'
+					title='Sistema para la gestión de carga de notas'
+				>
+					<Logo classStyle='login__logoIcon' />
+				</h1>
 				{loading ? 'Cargando...' : <IconUser />}
 			</div>
 			<BtnTheme classStyle='login__btnTheme' />
